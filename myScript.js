90d45a36a4a87f10c557c2554407eec8a9c65f5c
abdullah-mohsen <abdullahhaa144@gmail.com>
2016-07-10 14:53:54 +0300

    add validation

--- myScript.js
+++ myScript.js
@@ -1,13 +1,25 @@
-  function display(Num){ 
-                Calculator.txt.value=Calculator.txt.value + Num;
-            }
+function display(Num){ 
+    Calculator.txt.value=Calculator.txt.value + Num;
+}
 
-            function clearForm(){
-                Calculator.txt.value='';
-            }
+function clearForm(){
+    Calculator.txt.value='';
+}
 
-            function getResult(){
-                var evalo = eval(Calculator.txt.value);
-                Calculator.txt.value= evalo;
-            }
-           
+function getResult(){
+    var result=validateForm();
+    if(result){
+        var evalo = eval(Calculator.txt.value);
+        Calculator.txt.value= evalo;
+    }
+}
+
+function validateForm() {
+    var x = document.forms["Calculator"]["txt"].value;
+   //var n = x.includes("++");
+    if (x == "++" || x== "" || x=="**" || x=="--"||n==false) {
+        alert("can not click double opretion in same time");
+        return false;
+    }
+    return true;
+ }
